6648c0a796434ad71e6f0b9f05409679fbee60b8
fabian ledezma <fabianledezma@gmail.com>
2025-06-05 15:12:23 -0400

    agregando funcion multiplicar

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicacion
 
 class CalculatorTest(unittest.TestCase):
    
@@ -12,3 +12,7 @@ class CalculatorTest(unittest.TestCase):
 
 def test_resta(self):
     self.assertEqual(resta(4,3),1)
+
+
+def test_multiplicacion(self):
+    self.assertEqual(multiplicacion(4,7),1)
